f2178b60ead0d10f87515e9b4313f321df4650da
Dominic Law <dominic.law79@gmail.com>
2021-11-24 22:15:41 +1100

    Setup layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import NavBar from './components/NavBar';
+
 function App() {
   return (
     <div>
-
+      <header>
+        <NavBar />
+      </header>
+      <main></main>
+      <footer></footer>
     </div>
   );
 }
