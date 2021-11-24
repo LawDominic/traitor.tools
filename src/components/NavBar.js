56b274b621eac135e004515db646679107b0edb9
Dominic Law <dominic.law79@gmail.com>
2021-11-25 00:41:11 +1100

    Add metamask functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import MM from '../images/mm-logo.svg';
+
+import { useMetaMask } from "metamask-react";
 
 function NavBar() {
+
+    const { status, connect, account, chainId } = useMetaMask();
+
     return (
         <div>
-            <nav>
+            <nav className="font-kanit">
                 <div class="max-w-6xl mx-auto px-4">
                     <div class="flex justify-between">
                         <div class="flex space-x-4">
                             <div>
                             <a href="#" class="flex items-center py-5 px-2 text-black hover:text-gray-900">
-                                <span class="font-kanit font-semibold text-4xl text-indigo-900">traitor</span>
+                                <span class="font-semibold text-4xl text-indigo-900">traitor</span>
                             </a>
                             </div>
                         </div>
 
                         <div class="hidden md:flex items-center space-x-1">
-                            <a href="" class="font-kanit py-5 px-3 text-xl">Link</a>
-                            <a href="" class="py-2 px-3 bg-yellow-400 hover:bg-yellow-300 text-yellow-900 hover:text-yellow-800 rounded transition duration-300">Signup</a>
-                            <img src={MM} class="h-12 w-12"/>
+                            <a href="" class="py-5 px-3 text-xl">Link</a>
+                            {(function() {
+                                switch(status) {
+                                    case 'notConnected':
+                                        return (
+                                            <button onClick={connect} className="text-yellow-500 text-semibold bg-yellow-100 hover:bg-yellow-300 rounded-full py-2 px-3 transition duration-300">Connect to a wallet</button>
+                                        )
+                                    case 'connecting':
+                                        return (
+                                            <span className="text-yellow-500 text-semibold bg-yellow-100 hover:bg-yellow-300 rounded-full py-2 px-3 transition duration-300">Connecting...</span>
+                                        )
+                                    case 'connected':
+                                        console.log(chainId)
+                                        return (
+                                            <div>
+                                                <span className="text-green-500 text-semibold bg-green-100 rounded-full py-2 px-3 transition duration-300">
+                                                    {account.substring(0,6)}...{account.substring(account.length-4)}
+                                                </span>
+                                            </div>
+                                        )
+                                }
+                                }
+                            )()}
                         </div>
 
                         <div class="md:hidden flex items-center">
