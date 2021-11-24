49488c623de8554722e36b4fc02ef551f2623043
Dominic Law <dominic.law79@gmail.com>
2021-11-25 10:21:19 +1100

    Add Router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { MetaMaskProvider } from "metamask-react";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { MetaMaskProvider } from "metamask-react";
+import { BrowserRouter } from "react-router-dom";
+
 ReactDOM.render(
     <React.StrictMode>
-      <MetaMaskProvider>
-        <App />
-      </MetaMaskProvider>
+      <BrowserRouter>
+        <MetaMaskProvider>
+          <App />
+        </MetaMaskProvider>
+      </BrowserRouter>
     </React.StrictMode>,
   document.getElementById('root')
 );
